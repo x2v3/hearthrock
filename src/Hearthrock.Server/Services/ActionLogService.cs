cc4e88a42e3203d8adf63d84be46155ebdea5ef8
jx wang <jx@x2v3.com>
2018-09-20 17:29:05 +0800

    add action log service.

---
+++ src/Hearthrock.Server/Services/ActionLogService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Hearthrock.Contracts;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+
+namespace Hearthrock.Server.Services
+{
+    public class ActionLogService
+    {
+        public ActionLogService(string connstr)
+        {
+            connString = connstr;
+        }
+
+        private MySqlConnection connection;
+        private string connString;
+        private const string InsertSql ="insert into playlog (`session`,`turn`,`jsondata`,`action`) values(@session,@turn,@jsondata,@action)"
+      
+
+        public int AddPlayLog(RockScene scene, RockAction action)
+        {
+            if (connection?.State != ConnectionState.Open)
+            {
+                connection=new MySqlConnection(connString);
+                connection.Open();
+            }
+            var cmd =new MySqlCommand(InsertSql,connection);
+            cmd.Parameters.AddWithValue("@session", scene.SessionId);
+            cmd.Parameters.AddWithValue("@turn", scene.Turn);
+            cmd.Parameters.AddWithValue("@jsondata", JsonConvert.SerializeObject(scene));
+            cmd.Parameters.AddWithValue("@action",JsonConvert.SerializeObject(action));
+            return cmd.ExecuteNonQuery();
+        }
+    }
+}
