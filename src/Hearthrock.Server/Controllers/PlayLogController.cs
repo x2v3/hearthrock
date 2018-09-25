effe3163fc0f5a017f163241302917f37cef1219
jx wang <jx@x2v3.com>
2018-09-25 11:34:21 +0800

    temp commit

---
+++ src/Hearthrock.Server/Controllers/PlayLogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hearthrock.Server.Controllers
+{
+    [Route("[controller]")]
+    public class PlayLogController:Controller
+    {
+        [Route("")]
+        public ViewResult Show()
+        {
+            return View("~/View/PlayInfo/Index.cshtml");
+        }
+    }
+}
