f0827a42053336795eaa7a6e7ce698cfda08e576
jx wang <jx@x2v3.com>
2018-09-29 13:49:28 +0800

    add api test controller

---
+++ src/Hearthrock.Server/Controllers/ApiTestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hearthrock.Server.Controllers
+{
+    [Route("[controller]")]
+    public class ApiTestController:Controller
+    {
+        public ApiTestController(ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger(this.GetType());
+        }
+
+        private ILogger logger;
+
+        [Route("")]
+        public ActionResult Test(string p)
+        {
+            logger.LogWarning(p);
+            return new ContentResult(){Content = p,ContentType = "plain/text"};
+        }
+    }
+}
