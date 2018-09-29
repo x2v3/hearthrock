2b3ed5533fe0cb6fb1c26a167e11107406d3943d
jx wang <jx@x2v3.com>
2018-09-29 15:04:30 +0800

    add play result api.

---
+++ src/Hearthrock.Server/Controllers/DataReportController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hearthrock.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hearthrock.Server.Controllers
+{
+    [Route("[controller]")]
+    public class DataReportController:Controller
+    {
+        public DataReportController(ILoggerFactory loggerFactory,ActionLogService actionLogService)
+        {
+            logger = loggerFactory.CreateLogger(this.GetType());
+            logDbService = actionLogService;
+        }
+
+        private ILogger logger;
+        private ActionLogService logDbService;
+
+        [Route("playresult")]
+        public ActionResult PlayResult(string session, bool win)
+        {
+            logger.LogWarning($"{session} game over:won? {win}");
+            logDbService.AddPlayResultAsyncNoResult(session,win);
+            return Ok();
+        }
+    }
+}
