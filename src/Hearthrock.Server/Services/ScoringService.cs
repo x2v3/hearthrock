9f11717fe21285e3cf2f0d1804a3a6f6a3338255
jx wang <jx@x2v3.com>
2018-10-25 09:50:35 +0800

    change classifier

--- src/Hearthrock.Server/Services/ScoringService.cs
+++ src/Hearthrock.Server/Services/ScoringService.cs
@@ -9,6 +9,9 @@ using Hearthrock.Contracts;
 using Hearthrock.Server.Score;
 using Microsoft.Extensions.Logging;
 using Microsoft.ML.Legacy;
+using Microsoft.ML.Legacy.Data;
+using Microsoft.ML.Legacy.Trainers;
+using Microsoft.ML.Legacy.Transforms;
 
 namespace Hearthrock.Server.Services
 {
@@ -50,15 +53,52 @@ namespace Hearthrock.Server.Services
         
         public void Train(bool swapPlayer=false)
         {
-            
             var dr = new MysqlDbDataReader(config.PlayDBConnectionString);
-            var t = new Trainer();
             var tmatch = dr.GetMatches(1500, 0);
             var data = Helper.MatchToSceneData(tmatch,swapPlayer);
             var testData = Helper.MatchToSceneData(dr.GetMatches(100, 1500),swapPlayer);
-            model = t.BuildAndTrain(data,testData);
+            model = BuildAndTrain(data,testData);
             var filePath = Path.Combine(Environment.CurrentDirectory, "score.model");
             model.WriteAsync(filePath);
+        }
+
+
+        public PredictionModel<SceneData, ScenePrediction> BuildAndTrain(List<SceneData> data, List<SceneData> testdata)
+        {
+            var cs = CollectionDataSource.Create(data);
+            var pipeline = new LearningPipeline();
+            pipeline.Add(cs);
+            pipeline.Add(new ColumnCopier(("win", "Label")));
+            pipeline.Add(new CategoricalOneHotVectorizer("selfHeroClass", "opHeroClass"));
+            pipeline.Add(new ColumnConcatenator("Features"
+                , "selfHeroClass"
+                , "opHeroClass"
+                , "round"
+                , "selfHeroHealth"
+                , "opHeroHealth"
+                , "selfMinionsHealth"
+                , "opMinionsHealth"
+                , "selfMinionsAttackDamage"
+                , "opMinionsAttackDamage"
+                , "selfHasWindFury"
+                , "opHasWindFury"
+                , "selfHasLifeSteal"
+                , "opHasLifeSteal"
+                , "selfTauntMinionsHealth"
+                , "opTauntMinionsHealth"
+                , "selfHeroAttackDamage"
+                , "opHeroAttackDamage"
+            ));
+            var classifier = new LogisticRegressionBinaryClassifier();
+
+            pipeline.Add(classifier);
+            model = pipeline.Train<SceneData, ScenePrediction>();
+
+            var eva = new Microsoft.ML.Legacy.Models.BinaryClassificationEvaluator();
+            var m = eva.Evaluate(model, CollectionDataSource.Create(testdata));
+            Console.WriteLine($"acc:{m.Accuracy}");
+            Console.WriteLine($"auc:{m.Auc}");
+            return model;
         }
 
         public void TrainAsync(bool swapPlayer=false)
